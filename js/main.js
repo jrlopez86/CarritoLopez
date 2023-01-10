82cd4d6a420eac01912e35a417b932813208e04c
Rafael <jrlopez.16286@gmail.com>
2023-01-10 16:26:38 +0100

    Se agrega DOM, Eventos y LS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 
-    //Listado de objetos (productos)
+    //Constructor de productos
     class VideoJuegos {
       constructor(id, nombre, categoria, caratula, precio, cantidad) {
         this.id = id;
@@ -12,61 +12,65 @@
       }
     }
 
-    //Creo productos y los almaceno en un array:
-  
-    const game1 = new VideoJuegos(1, 'Super Mario World', 'Plataforma', src="./img/supermario.jpg",  160, 10);
-    const game2 = new VideoJuegos(2, 'Castlevania', 'Plataforma', src="./img/castlevania.jpg", 250, 6);
-    const game3 = new VideoJuegos(3, 'Zelda','Aventura', src="./img/zelda.jpg", 79,  2);
-    const game4 = new VideoJuegos(4, 'Star Fox','Shooter',src="./img/starfox.jpg", 32,  8);
-    const game5 = new VideoJuegos(5, 'Super Mario Kart', 'Carreras', src="./img/mariokart.jpg", 60, 5);
-    const game6 = new VideoJuegos(6, 'Super Metroid', 'Accion', src="./img/metroid.jpg", 337, 1);
+    //Creo productos y los almaceno en el array Listado:
+    const game1 = new VideoJuegos(1, 'Super Mario World', 'plataforma', src="./img/supermario.jpg",  160, 10);
+    const game2 = new VideoJuegos(2, 'Castlevania', 'plataforma', src="./img/castlevania.jpg", 250, 6);
+    const game3 = new VideoJuegos(3, 'Zelda','aventura', src="./img/zelda.jpg", 79,  2);
+    const game4 = new VideoJuegos(4, 'Star Fox','shooter',src="./img/starfox.jpg", 32,  8);
+    const game5 = new VideoJuegos(5, 'Super Mario Kart', 'carreras', src="./img/mariokart.jpg", 60, 5);
+    const game6 = new VideoJuegos(6, 'Super Metroid', 'accion', src="./img/metroid.jpg", 337, 1);
+    const game7 = new VideoJuegos(7, 'Aladín','plataforma',src="./img/aladin.jpg", 87,  3);
+    const game8 = new VideoJuegos(8, 'TMNT', 'plataforma', src="./img/tmnt.jpg", 250, 2);
+    const game9 = new VideoJuegos(9, 'Street Fighter II', 'lucha', src="./img/streetfighters.jpg", 200, 1);
+    const game10 = new VideoJuegos(10, 'Killer Instinct','lucha',src="./img/ki.jpg", 60,  2);
+    const game11 = new VideoJuegos(11, 'Batman Returns', 'accion', src="./img/batman.jpg", 149, 4);
+    const game12 = new VideoJuegos(12, 'The lion king', 'aventura', src="./img/tlk.jpg", 282, 1);
+    const game13 = new VideoJuegos(13, 'X-MEN: Mutant Apocalypse','accion',src="./img/xmen.jpg", 91,  7);
+    const game14 = new VideoJuegos(14, 'Donkey Kong Country', 'plataforma', src="./img/dkk.jpg", 60, 6);
+    const game15 = new VideoJuegos(15, 'EarthBound', 'rol', src="./img/eb.jpg", 1000, 1);
 
 
     //Array de objetos (productos)
-    const Listado =  [game1, game2, game3, game4, game5, game6];
+    const Listado =  [game1, game2, game3, game4, game5, game6, game7, game8, game9, game10, game11, game12, game13, game14, game15];
     let carrito = [];
 
-        // Se agrega localstorage
-        document.addEventListener('DOMContentLoaded', () => {
-          if (localStorage.getItem('carrito')){
-              carrito = JSON.parse(localStorage.getItem('carrito'))
-              actualizarCarrito()
-          }
-      })
-
+    // Se agrega localstorage y JSON
+    document.addEventListener('DOMContentLoaded', () => {
+      if (localStorage.getItem('carrito')){
+          carrito = JSON.parse(localStorage.getItem('carrito'))
+          actualizarCarrito()
+      }
+  })
 
     //Se crean las cards de los productos al html
     const contProduct= document.querySelector('#productos')
 
-    function crearHtml(){
+    function crearHtml(arr) {
+      let li;
+      contProduct.innerHTML="";
+      for (const producto of arr) {
+         li = document.createElement('li')
+            li.innerHTML = `<div class='card'>
+                                <img src="${producto.caratula}"></img>
+                                <h3 class="card__titulo">${producto.nombre}</h3>
+                                <p>Precio <span class="card__precio">$${producto.precio}</span></p>
+                                <button id="btn${producto.id}" class="btn btn-primary"> Agregar al Carrito </button>
+                            </div>`;
+                            
+          contProduct.appendChild(li);
+  
+        //Captura el id del producto a travez del boton
+        const boton = document.getElementById(`btn${producto.id}`);
+        boton.addEventListener('click', () => {
+          agregarAlCarrito(producto.id)
+  
+        });
     
-    for (const producto of Listado) {
-
-    let li = document.createElement('li')
-        li.innerHTML = `<div class='card'>
-                            <img src="${producto.caratula}"></img>
-                            <h3 class="card__titulo">${producto.nombre}</h3>
-                            <p>Precio <span class="card__precio">$${producto.precio}</span></p>
-                            <button id="btn${producto.id}" class="btn btn-primary"> Agregar al Carrito </button>
-                        </div>`;
-                        
-      contProduct.appendChild(li);
-
-
-     
-    //Captura el id del producto a travez del boton
-    const boton = document.getElementById(`btn${producto.id}`);
-    boton.addEventListener('click', () => {
-      agregarAlCarrito(producto.id)
-
-      
-    });
-
-  };
-}
-crearHtml()
-
-
+      };
+  
+    }
+    crearHtml(Listado)
+    
 
 //Puseha el producto al array carrito
 const agregarAlCarrito = (id) => {
@@ -76,7 +80,7 @@ const agregarAlCarrito = (id) => {
   actualizarCarrito()
 }
 
-//Se agrega los productos al html carrito de manera dinamica
+//Se agrega los productos al carrito de manera dinamica
 const contenedorCarrito = document.querySelector("#lista__carrito tbody")
 function actualizarCarrito() {
   //Limpiar el HTML
@@ -99,11 +103,9 @@ function actualizarCarrito() {
                       ; 
 
       contenedorCarrito.appendChild(tr);
-         //Se agrega seteo
-         localStorage.setItem('carrito', JSON.stringify(carrito))
+      //Seteo LS 
+      localStorage.setItem('carrito', JSON.stringify(carrito))
       calcularTotalCompra();
-      calcularTotalmodal();
-   
   });
   
 }
@@ -122,33 +124,23 @@ vaciarCarrito.addEventListener('click', () => {
 
 //Calcula el total de la compra
 const totalCompra = document.getElementById('totalCompra');
+const modalTotal = document.getElementById('modal__total');
 const calcularTotalCompra = () => {
   let total = 0;
   carrito.forEach((producto) => {
     total += producto.precio;
   });
   totalCompra.innerHTML = total;
-
-};
-
-//Calcula el total de la compra (modal)
-const modalTotal = document.getElementById('modal__total');
-const calcularTotalmodal = () => {
-  let total = 0;
-  carrito.forEach((producto) => {
-    total += producto.precio;
-  });
   modalTotal.innerHTML = total;
-  
-
 };
 
 //Funcion para borrar el Precio total cuando se vacia el carrito
 function VaciarPrecio () {
   vaciarCarrito.addEventListener('click', () => {
+    localStorage.removeItem("carrito");
     totalCompra.innerHTML = 0;
     actualizarCarrito();
-    calcularTotalmodal();
+    calcularTotalCompra();
   });
 }
 VaciarPrecio () 
@@ -161,8 +153,9 @@ const modalParDire = document.getElementById('modal__par-2');
 const modalParNom = document.getElementById('modal__par-1');
 const btnComprar = document.querySelector("#modal__btn");
 const textCompra = document.getElementById("modal__compra");
+const textCompraError = document.getElementById("modal__compra-error");
 const btnBorrar = document.getElementById("modal__btn-borrar");
-
+const btnCerrar = document.getElementById('moda__btn-cerrar')
 
 //Cuando se hace click en comprar se ejecuta la funcion
 btnComprar.addEventListener("click", ()=>{ 
@@ -170,13 +163,11 @@ btnComprar.addEventListener("click", ()=>{
 });
 
 function condicionForm() {
-
   let inputNombre = document.getElementById("modal__nombre").value
   let inputDire = document.getElementById("modal__dire").value
   let inputTarjeta = document.getElementById("modal__tarjeta").value
   let inputCvv = document.getElementById("modal__cvv").value
-  const textCompraError = document.getElementById("modal__compra-error");
-
+ 
    //Condiciones en el formulario de compra
 
    if (inputNombre.length === 0) {
@@ -203,7 +194,7 @@ function condicionForm() {
     modalParCvv.innerText = " ";
     modalTotal.innerHTML = 0;
     totalCompra.innerHTML = 0;
-    
+  }
 
   //Se valida si hay productos, el usuario no puede comrpar si no hay nada en el carrito
   if(carrito.length === 0){
@@ -217,18 +208,20 @@ function condicionForm() {
     }
     actualizarCarrito();
   
-  }
-
-  
   
   formularioReset.reset();
 
 }
 
+//Se borra el texto "Gracias por su compra e Ingresa un producto" al darle click a cerrar
+btnCerrar.addEventListener('click', () =>{
+  textCompra.innerText = " ";
+  textCompraError.innerText = " ";
+})
+
 //Borra los datos del formulario
 function borrarFormulario() {
   btnBorrar.addEventListener("click", ()=>{
-  
     textCompra.innerText = " ";
     formularioReset.reset();
    
@@ -236,21 +229,25 @@ function borrarFormulario() {
 }
 borrarFormulario()
 
-
-//filtrar videojuego
+//Filtra los juegos por categoria
+const search = document.querySelector("#search")
 function filtrarJuego(filtro) {
-  let filtrado = Listado.filter((el) =>{
-    return el.categoria.includes(filtro)
+  let filtrado = Listado.filter((producto) =>{
+    return producto.categoria.includes(filtro)
   });
   return filtrado
-
 }
 
-//Filtar por nombre
+//Llama a la funcion
+crearHtml(Listado)
+//Filtra por categoria 
 search.addEventListener("input", () => {
   let filtro = filtrarJuego(search.value)
   crearHtml(filtro)
 });
+
+
+
 
 
 
